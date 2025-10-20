e595975c55496611c7a5a42f973ce31e65902211
Toufiqul Islam Anim <toufiq9610@gmail.com>
2025-10-20 17:56:06 +0600

    frontend and backend folder structure created

---
+++ job-assistant-server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Route is working");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+export default app;
